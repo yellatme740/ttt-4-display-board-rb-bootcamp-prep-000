64733f73e242b8fc4778e5d8104940d07df42705
Rach <yellatme740@hotmail.com>
2017-10-18 21:37:47 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,6 +5,6 @@ def display_board(array)
   puts "#{array[0]} |  #{array[1]} | #{array[2]}"
   puts divider
   puts "#{array[3]} | #{array[4]} | #{array[5]}"
-  puts divider 
+  puts divider
   puts "#{array[6]} | #{array[7]} | #{array[8]}"
 end
